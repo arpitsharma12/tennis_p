30c4b63fd686f262ef432a5358cc5081cd7bb593
Arpit Sharma <84865895+arpitsharma12@users.noreply.github.com>
2022-04-05 09:21:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
+
+let arr = ["Roger", "Rafa", "Novak"];
+let rev = arr.reverse();
+
+rev.forEach(elem => AddP(elem));
+
+
+function AddP(name) {
+  let box = document.getElementById("box");
+  let li = document.createElement("li");
+
+
+  li.textContent = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  li.className = "list"
+  let a = document.createElement("a");
+  a.textContent = "x";
+  a.href = "javascript:void(0)";
+  a.className = "remove";
+  li.appendChild(a);
+  let pos = box.firstElementChild;
+  if (pos == null) {
+    box.appendChild(li);
+  } else {
+    box.insertBefore(li, pos);
+  }
+}
+
 function addMore() {
+
   document.getElementById("error").innerHTML = "";
   let name = document.getElementById("name").value;
   if (name == "") {
     document.getElementById("error").innerHTML = "please enter value";
   } else {
-    let box = document.getElementById("box");
-    let li = document.createElement("li");
-    li.textContent = name;
-    let a = document.createElement("a");
-    a.textContent = "x";
-    a.href = "javascript:void(0)";
-    a.className = "remove";
-    li.appendChild(a);
-    let pos = box.firstElementChild;
-    if (pos == null) {
-      box.appendChild(li);
-    } else {
-      box.insertBefore(li, pos);
-    }
+    AddP(name);
+
+
   }
   document.getElementById("name").value = "";
 }
@@ -31,3 +48,27 @@ function myfunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+
+function searches() {
+  let input = document.getElementById('search').value
+  input = input.toUpperCase();
+  let x = document.getElementsByClassName('list');
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toUpperCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
+
+var input = document.getElementById("name");
+input.addEventListener("keyup", function (event) {
+
+  if (event.key === `Enter`) {
+    addMore();
+  }
+});
